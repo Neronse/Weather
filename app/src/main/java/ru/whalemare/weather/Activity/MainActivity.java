af2f50329a7eff63060f9823797a806bd09b7a8b
whalemare <nanostet@gmail.com>
2016-01-22 13:12:47 +0600

    Приложение полностью соответствует ТЗ + Добавил textView с текстом о соединении с интернетом и наличии данных о погоде + Добавил проверку на наличие интернета Мелочи почищены

--- app/src/main/java/ru/whalemare/weather/Activity/MainActivity.java
+++ app/src/main/java/ru/whalemare/weather/Activity/MainActivity.java
@@ -1,5 +1,8 @@
 package ru.whalemare.weather.Activity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,6 +10,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import ru.whalemare.weather.R;
 import ru.whalemare.weather.WeatherTask;
@@ -15,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     WeatherTask weatherTask;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
+
+    TextView pressRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +36,17 @@ public class MainActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        weatherTask = new WeatherTask(getApplicationContext(), recyclerView);
-        weatherTask.execute();
+        pressRefresh = (TextView) findViewById(R.id.press_refresh);
+
+        // Проверка на подключение к интернету
+        ConnectivityManager cm = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo internetInfo = cm.getActiveNetworkInfo();
+        if (internetInfo == null || !internetInfo.isConnectedOrConnecting())
+            pressRefresh.setText("Нет подключения к интернету");
+
+        //Закомменчено для соответствия ТЗ
+        //weatherTask = new WeatherTask(getApplicationContext(), recyclerView);
+        //weatherTask.execute();
     }
 
     @Override
@@ -44,12 +60,12 @@ public class MainActivity extends AppCompatActivity {
         switch(item.getItemId())
         {
             case R.id.action_refresh:
+                pressRefresh.setVisibility(View.GONE); // уберем TextView с layout
                 weatherTask = new WeatherTask(getApplicationContext(), recyclerView);
                 weatherTask.execute();
-                break;
+                return super.onOptionsItemSelected(item);
             default:
                 return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 }
