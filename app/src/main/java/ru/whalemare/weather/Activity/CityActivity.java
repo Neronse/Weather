959ec2f218104bcf1db34fe7e7ba9694058f96f1
whalemare <nanostet@gmail.com>
2016-03-16 17:48:55 +0600

    Initial commit created CityActivity where will be placed RecyclerView with list of cities

---
+++ app/src/main/java/ru/whalemare/weather/Activity/CityActivity.java
@@ -0,0 +1,15 @@
+package ru.whalemare.weather.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ru.whalemare.weather.R;
+
+public class CityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city);
+    }
+}
