5adab5baa479e7d2cb32d53c2b191da5e5ec1edc
whalemare <nanostet@gmail.com>
2016-03-21 15:32:39 +0600

    Fixed a theme for the activity from the list of cities  => deleted toolbar from fragment_city.xml

--- app/src/main/java/ru/whalemare/weather/Fragments/CityFragment.java
+++ app/src/main/java/ru/whalemare/weather/Fragments/CityFragment.java
@@ -3,12 +3,10 @@ package ru.whalemare.weather.Fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -58,10 +56,6 @@ public class CityFragment extends Fragment implements SearchView.OnQueryTextList
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_city, container, false);
 
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar_city);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-        toolbar.setLogo(R.mipmap.ic_launcher);
-
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_city);
         layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
@@ -89,6 +83,7 @@ public class CityFragment extends Fragment implements SearchView.OnQueryTextList
         final MenuItem item = menu.findItem(R.id.action_search);
         final SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
     }
+
 
 
     @Override
