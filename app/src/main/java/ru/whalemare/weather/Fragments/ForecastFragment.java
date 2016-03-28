3f286c03607e687b0e5ec8927b984614633a1a97
whalemare <nanostet@gmail.com>
2016-03-28 15:35:47 +0600

    Fixes-commit because the branches started in the wrong place

--- app/src/main/java/ru/whalemare/weather/Fragments/ForecastFragment.java
+++ app/src/main/java/ru/whalemare/weather/Fragments/ForecastFragment.java
@@ -81,7 +81,6 @@ public class ForecastFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setHasOptionsMenu(true);
         if (getArguments() != null) {
             this.weatherCode = getArguments().getString(KEY_WEATHER, null);
