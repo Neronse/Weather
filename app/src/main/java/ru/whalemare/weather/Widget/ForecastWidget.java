5d0f8c606eb57e9e6f7291e8f9e5339fb23c5ae9
whalemare <nanostet@gmail.com>
2016-04-22 15:36:05 +0600

    Widget can be refreshed

--- app/src/main/java/ru/whalemare/weather/Widget/ForecastWidget.java
+++ app/src/main/java/ru/whalemare/weather/Widget/ForecastWidget.java
@@ -1,12 +1,13 @@
 package ru.whalemare.weather.Widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.util.Log;
 import android.widget.RemoteViews;
 
 import java.util.Random;
@@ -21,8 +22,6 @@ import ru.whalemare.weather.database.CitiesProvider;
 
 public class ForecastWidget extends AppWidgetProvider {
 
-    private final static String TAG = "ForecastWidget";
-    private final String ACTION_WIDGET_UPDATE = "ActionWidgetUpdate";
     private static SharedPreferences shared;
     static String stringTod,
             humanTod,
@@ -38,16 +37,20 @@ public class ForecastWidget extends AppWidgetProvider {
             "вечером"
     };
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    static void updateAppWidget(Context context) {
 
-        Log.d(TAG, "updateAppWidget");
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+        Intent updateIntent = new Intent(context, ForecastWidget.class);
+        updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, updateIntent, 0);
 
         shared = context.getSharedPreferences(CitiesProvider.CitiesMetaData.KEY_GISMETEO_CODE, Context.MODE_PRIVATE);
 
         gismeteoCode = shared.getString(CitiesProvider.CitiesMetaData.KEY_GISMETEO_CODE, null);
         cityName = shared.getString(CitiesProvider.CitiesMetaData.KEY_CITY_NAME, null);
 
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.forecast_widget);
         if (gismeteoCode != null) {
             Cursor cursor = context.getContentResolver().query(CitiesProvider.STATS_CONTENT_URI, null, CitiesProvider.StatsMetaData.KEY_GISMETEO_CODE + " = ?", new String[]{gismeteoCode}, CitiesProvider.StatsMetaData.KEY_DATE);
 
@@ -60,54 +63,38 @@ public class ForecastWidget extends AppWidgetProvider {
                 stringTod = cursor.getString(cursor.getColumnIndex(CitiesProvider.StatsMetaData.KEY_TOD));
                 humanTod = tods[Integer.parseInt(stringTod)];
 
-
                 cursor.close();
             }
+
         } else {
             cityName = "Город не выбран";
+            views.setViewVisibility(R.id.textview_widget_temperature, 0);
         }
+        views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
 
-        Log.d(TAG, "updateAppWidget: gismeteoCode = " + gismeteoCode);
-        Log.d(TAG, "updateAppWidget: cityName = " + cityName);
-        Log.d(TAG, "updateAppWidget: tempr = " + temperature);
-
-
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.forecast_widget);
         views.setTextViewText(R.id.textview_widget_city_name, cityName);
         views.setTextViewText(R.id.textview_widget_temperature, Integer.toString(temperature) + context.getString(R.string.celcium));
         views.setTextViewText(R.id.textview_widget_tod, humanTod);
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        ComponentName thisWidget = new ComponentName(context, ForecastWidget.class);
+
+        appWidgetManager.updateAppWidget(thisWidget, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-        Intent intent = new Intent(context, ForecastWidget.class);
-        intent.setAction(ACTION_WIDGET_UPDATE);
-
-
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateAppWidget(context);
         }
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
+        if (intent.getAction().equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
+            updateAppWidget(context);
+        }
     }
 
 }
